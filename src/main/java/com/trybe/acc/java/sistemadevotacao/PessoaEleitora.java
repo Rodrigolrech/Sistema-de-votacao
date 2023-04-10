990033500b75826d1e1dcc6c4e14aeba7b3c1c64
Rodrigo Luiz Rech <rodrigorech92@gmail.com>
2023-04-10 19:57:28 -0300

    Feat: PessoaEleitora

--- src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,29 @@
 package com.trybe.acc.java.sistemadevotacao;
 
-public class PessoaEleitora {
+public class PessoaEleitora extends Pessoa {
 
+  private String cpf;
+
+  public PessoaEleitora(String nome, String cpf) {
+    setNome(nome);
+    this.cpf = cpf;
+  }
+
+  @Override
+  public String getNome() {
+    return null;
+  }
+
+  @Override
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }
