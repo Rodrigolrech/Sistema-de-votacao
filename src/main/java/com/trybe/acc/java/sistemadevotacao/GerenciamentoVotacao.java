9bf99fe033d017679e50cf96dc273fc985f06e42
Rodrigo Luiz Rech <rodrigorech92@gmail.com>
2023-04-10 21:11:42 -0300

    Feat: Principal

--- src/main/java/com/trybe/acc/java/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,5 +1,98 @@
 package com.trybe.acc.java.sistemadevotacao;
 
-public class GerenciamentoVotacao {
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
+public class GerenciamentoVotacao {
+  private ArrayList<PessoaCandidata> pessoasCandidatas;
+  private ArrayList<PessoaEleitora> pessoasEleitoras;
+  private ArrayList<String> cpfComputado;
+  int totalVotos;
+
+  public GerenciamentoVotacao() {
+    pessoasEleitoras = new ArrayList<>();
+    pessoasCandidatas = new ArrayList<>();
+    cpfComputado = new ArrayList<>();
+    totalVotos = 0;
+  }
+
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+    boolean exist = false;
+    for (PessoaCandidata pessoaCandidata : pessoasCandidatas) {
+      if (pessoaCandidata.getNumero() == numero) {
+        exist = true;
+      }
+    }
+    if (exist) {
+      System.out.println("Número pessoa candidata já utilizado!");
+    } else {
+      PessoaCandidata pc = new PessoaCandidata(nome, numero);
+      pessoasCandidatas.add(pc);
+    }
+  }
+
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+    boolean exist = false;
+    for (PessoaEleitora pessoaEleitora : pessoasEleitoras) {
+      if (Objects.equals(pessoaEleitora.getCpf(), cpf)) {
+        exist = true;
+        break;
+      }
+    }
+    if (exist) {
+      System.out.println("Pessoa eleitora já cadastrada!");
+    } else {
+      PessoaEleitora pe = new PessoaEleitora(nome, cpf);
+      pessoasEleitoras.add(pe);
+    }
+  }
+
+  public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
+    boolean exist = false;
+    for (String cpf : cpfComputado) {
+      if (Objects.equals(cpfPessoaEleitora, cpf)) {
+        exist = true;
+        break;
+      }
+    }
+    if (exist) {
+      System.out.println("Pessoa eleitora já votou!");
+    } else {
+      for (PessoaCandidata pessoaCandidata : pessoasCandidatas) {
+        if (pessoaCandidata.getNumero() == numeroPessoaCandidata) {
+          pessoaCandidata.receberVoto();
+          cpfComputado.add(cpfPessoaEleitora);
+          totalVotos++;
+        }
+      }
+    }
+  }
+
+  public void mostrarResultado() {
+    if (totalVotos == 0) {
+      System.out.println("É preciso ter pelo menos um voto para mostrar o resultado");
+    } else {
+      for (PessoaCandidata pc : pessoasCandidatas) {
+        String stringBuild =
+            new StringBuilder()
+                .append("Nome: ")
+                .append(pc.getNome())
+                .append(" - ")
+                .append(pc.getVotos())
+                .append(" votos ( ")
+                .append(String.format("%.1f", calcularPorcentagemVotos(pc)))
+                .append("% )")
+                .toString();
+        System.out.println(stringBuild);
+      }
+    }
+    String stringBuilderTotal =
+        new StringBuilder().append("Total de votos: ").append(totalVotos).toString();
+    System.out.println(stringBuilderTotal);
+  }
+
+  private double calcularPorcentagemVotos(PessoaCandidata pc) {
+    return Math.round(((double) pc.getVotos() / (double) totalVotos) * 100);
+  }
 }
--- src/main/java/com/trybe/acc/java/sistemadevotacao/Principal.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/Principal.java
@@ -1,10 +1,76 @@
 package com.trybe.acc.java.sistemadevotacao;
 
+import java.util.Scanner;
+
 public class Principal {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    System.out.println("----------- Bem-vindo ao Sistema de Votação -----------");
+    GerenciamentoVotacao gv = new GerenciamentoVotacao();
+    Scanner scanner = new Scanner(System.in);
+    boolean cadastrarPessoaCandidata;
+    do {
+      System.out.println("Cadastrar pessoa candidata?");
+      System.out.println("1 - Sim");
+      System.out.println("2 - Não");
+      System.out.println("Entre com o número correspondente à opção desejada:");
+      int cadastrar = scanner.nextInt();
+      if (cadastrar == 1) {
+        cadastrarPessoaCandidata = true;
+        System.out.println("Entre com o nome da pessoa candidata:");
+        String nome = scanner.next();
+        System.out.println("Entre com o número da pessoa candidata:");
+        int numero = scanner.nextInt();
+        gv.cadastrarPessoaCandidata(nome, numero);
+      } else {
+        cadastrarPessoaCandidata = false;
+      }
+    } while (cadastrarPessoaCandidata);
+    System.out.println("");
+    System.out.println("----------- Cadastre as pessoas eleitoras -----------");
+    boolean cadastrarPessoaEleitora;
 
+    do {
+      System.out.println("Cadastrar pessoa eleitora?");
+      System.out.println("1 - Sim");
+      System.out.println("2 - Não");
+      System.out.println("Entre com o número correspondente à opção desejada:");
+      int cadastrarEleitor = scanner.nextInt();
+      if (cadastrarEleitor == 1) {
+        cadastrarPessoaEleitora = true;
+        System.out.println("Entre com o nome da pessoa eleitora:");
+        String nomeEleitor = scanner.next();
+        System.out.println("Entre com o cpf da pessoa eleitora:");
+        String cpf = scanner.next();
+        gv.cadastrarPessoaEleitora(nomeEleitor, cpf);
+      } else {
+        cadastrarPessoaEleitora = false;
+      }
+    } while (cadastrarPessoaEleitora);
+
+    System.out.println("");
+    System.out.println("----------- Votação iniciada! -----------");
+    boolean continuarVotacao;
+    do {
+      System.out.println("Entre com o número correspondente à opção desejada:");
+      System.out.println("1 - Votar");
+      System.out.println("2 - Resultado Parcial");
+      System.out.println("3 - Finalizar Votação");
+      int opcao = scanner.nextInt();
+      if (opcao == 1) {
+        continuarVotacao = true;
+        System.out.println("Entre com o cpf da pessoa eleitora:");
+        String cpf2 = scanner.next();
+        System.out.println("Entre com o número da pessoa candidata:");
+        int numeroCandidato = scanner.nextInt();
+        gv.votar(cpf2, numeroCandidato);
+      } else if (opcao == 2) {
+        continuarVotacao = true;
+        gv.mostrarResultado();
+      } else {
+        continuarVotacao = false;
+        gv.mostrarResultado();
+      }
+    } while (continuarVotacao);
   }
-
 }
